c3604a6a95c7e7bf698305467dafe1314e75042e
Maria DelSignore <maria.ad27@gmail.com>
2021-10-17 16:40:57 -0600

    Add attributes to title form

--- src/components/pages/Dashboard/TitleForm/TitleForm.tsx
+++ src/components/pages/Dashboard/TitleForm/TitleForm.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const TitleForm = () => {
+  const [title, setTitle] = useState('')
 
   return (
     <form>
-      <input />
+      <input 
+        aria-label='title field'
+        placeholder='Film Title'
+        type='text'
+        name='title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <button>Create EPK</button>
     </form>
   )
 }
