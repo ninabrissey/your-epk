82b6210f5de974addcad7c8defa4e0de744983bf
Maria DelSignore <maria.ad27@gmail.com>
2021-10-17 16:41:37 -0600

    Render EPK container

---
+++ src/components/pages/Dashboard/EPKContainer/EPKContainer.tsx
@@ -0,0 +1,14 @@
+import EPKCard from "../EPKCard/EPKCard";
+
+const EPKContainer = () => {
+
+  return(
+    <div>
+      <p>this is the EPK container, woot</p>
+      <EPKCard />
+    </div>
+  )
+}
+
+
+export default EPKContainer;
