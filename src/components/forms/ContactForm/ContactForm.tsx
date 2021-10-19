aa15933377d90e20db2e1e3221255b879c6b67be
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 22:43:45 -0600

    Import MUI components to render input fields

--- src/components/forms/ContactForm/ContactForm.tsx
+++ src/components/forms/ContactForm/ContactForm.tsx
@@ -1,11 +1,22 @@
 import { useState, useEffect} from 'react'
 import { film, header } from "../../../utils/mockData";
 
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+
 const ContactForm = () => {
 
   return (
     <section className='contact-form-container'>
-      <p>I am the contact form</p>
+      <h2>Contact</h2>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Company" variant="outlined" />
+        <Button variant="text">save</Button>
+      </FormControl>
     </section>
   )
 }
