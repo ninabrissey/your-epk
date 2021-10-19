31229aa8d9fe1500bfbfbe7c546baf456c6c70dd
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 19:00:28 -0600

    Build base component structure

--- src/components/forms/HeaderForm/HeaderForm.tsx
+++ src/components/forms/HeaderForm/HeaderForm.tsx
@@ -1,4 +1,14 @@
-const HeaderForm = () => {}
+
+
+const HeaderForm = () => {
+
+  return (
+    <section className='header-container'>
+      <p>film title goes here</p>
+      <p>I am the header form</p>
+    </section>
+  )
+}
 
 
 export default HeaderForm;
