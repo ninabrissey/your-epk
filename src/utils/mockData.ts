aa6307deda5fd57b6105b2b7a12b5177d05110bf
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 19:42:30 -0600

    Create mock data

---
+++ src/utils/mockData.ts
@@ -0,0 +1,13 @@
+export const filmData = {
+  title: 'Amazing Grace',
+  header: {
+    img: 'https://www.notion.so/Amazing-Grace-Data-4a7aa209ca824ce6b1e80708104ea88c#cbbbda1ed39b442cac4ef55c5f9782c8',
+    description: 'Grace, who is Ed\'s only hope, has been enabling her father\'s drinking for far too long. Ed, a Vietnam Veteran, is starting to unravel. Both must learn how to cope in this imperfect world.'
+  },
+  contactInfo: {
+    name: 'Nina Brissey',
+    phoneNumber: '774-308-5555',
+    email: 'nina_brissey@gmail.com',
+    company: 'Positude Productions and Frost Entertainment'
+  }
+}
