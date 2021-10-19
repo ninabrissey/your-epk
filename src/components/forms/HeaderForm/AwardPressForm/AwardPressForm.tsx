8c21c090bfa242bc6cf3c4e8922dbc0ff4203624
Nina Brissey <nbrissey@gmail.com>
2021-10-19 16:20:04 -0600

    Add/work with inline styling react mui

--- src/components/forms/HeaderForm/AwardPressForm/AwardPressForm.tsx
+++ src/components/forms/HeaderForm/AwardPressForm/AwardPressForm.tsx
@@ -32,11 +32,12 @@ const AwardPressForm = () => {
   // });
   
   return (
-    <section className="press-awards-form">
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-      <InputLabel id="award-or-press">type</InputLabel>
-
+    <section>
+      <FormControl style={{ height: '50vh'}} sx={{ m: 1, minWidth: 120 }}>
+        <div className="press-awards-form" >
+        <InputLabel id="award-or-press">type</InputLabel>
        <Select
+          style={{ width: '20%', minWidth: '85px', marginBottom: '3%'}}
           labelId="award-or-press"
           id="award-or-press"
           // onChange={handleChange}
@@ -45,10 +46,11 @@ const AwardPressForm = () => {
           <MenuItem value={'award'}>award</MenuItem>
           <MenuItem value={'press'}>press</MenuItem>
         </Select>
-      <TextField id="outlined-basic" label="publication" variant="outlined" />
-      <TextField id="outlined-basic" label="link" variant="outlined" />
-      <TextField id="outlined-basic" label="quote" variant="outlined" />
-      <Button variant="text">save</Button>
+      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="publication" variant="outlined" />
+      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="link" variant="outlined" />
+      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="quote" variant="outlined" />
+      <Button style={{ background: 'orange', height: '57px'}} variant="text">save</Button>
+      </div>
       </FormControl>
     </section>
   )
