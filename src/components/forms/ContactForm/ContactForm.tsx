18b3a49fa89954766ada51498e45c3f5c2f0efe8
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 19:25:50 -0600

    Add pseudocode for what file needs

--- src/components/forms/ContactForm/ContactForm.tsx
+++ src/components/forms/ContactForm/ContactForm.tsx
@@ -1,7 +1,7 @@
 const ContactForm = () => {
 
   return (
-    <section className='contact-container'>
+    <section className='contact-form-container'>
       <p>I am the contact form</p>
     </section>
   )
@@ -9,3 +9,11 @@ const ContactForm = () => {
 
 
 export default ContactForm;
+
+
+// CONTACT FORM
+// -needs input for name, phone number, email, company
+// -needs edit btn
+// -needs save btn
+
+// renders inside the header component
--- src/components/forms/HeaderForm/HeaderForm.tsx
+++ src/components/forms/HeaderForm/HeaderForm.tsx
@@ -3,7 +3,7 @@
 const HeaderForm = () => {
 
   return (
-    <section className='header-container'>
+    <section className='header-form-container'>
       <p>film title goes here</p>
       <p>I am the header form</p>
     </section>
@@ -12,3 +12,19 @@ const HeaderForm = () => {
 
 
 export default HeaderForm;
+
+
+
+// HEADER FORM
+// -needs input for description and img
+// -needs an edit btn
+// -needs a save btn
+
+// renders img across the entire page
+// there will be a container on top of the img 
+  // -displays the film title and description
+    // -title is passed from parent/from network request
+    // -description is rendered from the form
+
+// the container that holds the title and description will also hold the contact component
+
