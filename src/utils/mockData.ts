c13713b6a6ad09ec660fa45f8bfa696c9d5bd863
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 21:16:49 -0600

    Add types to mock data

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,13 +1,25 @@
-export const filmData = {
-  title: 'Amazing Grace',
-  header: {
-    img: 'https://www.notion.so/Amazing-Grace-Data-4a7aa209ca824ce6b1e80708104ea88c#cbbbda1ed39b442cac4ef55c5f9782c8',
-    description: 'Grace, who is Ed\'s only hope, has been enabling her father\'s drinking for far too long. Ed, a Vietnam Veteran, is starting to unravel. Both must learn how to cope in this imperfect world.'
-  },
-  contactInfo: {
-    name: 'Nina Brissey',
-    phoneNumber: '774-308-5555',
-    email: 'nina_brissey@gmail.com',
-    company: 'Positude Productions and Frost Entertainment'
-  }
+export const filmData : {
+  title: string;
+} = {
+  title: 'Amazing Grace'
+}
+
+export const headerData : {
+  img: string;
+  description: string;
+  } = {
+  img: 'https://images.squarespace-cdn.com/content/v1/594ea6feccf210e3df36ce8d/1530550829458-1E1T99Y53C81CU09S9P3/Nina+Brissey+and+Robert+Craighead+Amazing+Grace+Film?format=1500w',
+  description: 'Grace, who is Ed\'s only hope, has been enabling her father\'s drinking for far too long. Ed, a Vietnam Veteran, is starting to unravel. Both must learn how to cope in this imperfect world.'
+}
+
+export const contactData : {
+  name: string;
+  phoneNumber: string;
+  email: string;
+  company: string;
+  } = {
+  name: 'Nina Brissey',
+  phoneNumber: '774-308-5555',
+  email: 'nina_brissey@gmail.com',
+  company: 'Positude Productions and Frost Entertainment'
 }
