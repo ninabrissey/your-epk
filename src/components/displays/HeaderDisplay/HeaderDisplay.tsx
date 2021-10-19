168c12c594b60b8beacb7c2cd697545172c1dffb
Maria DelSignore <maria.ad27@gmail.com>
2021-10-18 21:16:36 -0600

    Import usestate and useeffect

--- src/components/displays/HeaderDisplay/HeaderDisplay.tsx
+++ src/components/displays/HeaderDisplay/HeaderDisplay.tsx
@@ -1,7 +1,18 @@
+import { useState, useEffect} from 'react'
+import { headerData } from "../../../utils/mockData";
+
 const HeaderDisplay = () => {
+  const [data1, setHeaderData1] = useState(headerData)
+
+  // useEffect(() => {
+  //   setHeaderData1(headerData)
+  // }, [])
+
+  console.log('headerData: ', data1)
 
   return (
     <section className='header-display-container'>
+      <img src={data1.img} />
       <p>I am the header displayed version</p>
     </section>
   )
