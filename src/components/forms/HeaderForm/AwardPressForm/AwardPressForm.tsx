d2edee769bf0a517bedcd1da3d36c17327b16da6
Nina Brissey <nbrissey@gmail.com>
2021-10-20 00:01:49 -0600

    Adjust styling, add AwardPressDisplay

--- src/components/forms/HeaderForm/AwardPressForm/AwardPressForm.tsx
+++ src/components/forms/HeaderForm/AwardPressForm/AwardPressForm.tsx
@@ -7,6 +7,7 @@ import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import {FilmEPK} from '../../../../types'
 import './AwardPressForm.scss'
+import AwardPressDisplay from '../../../displays/AwardPressDisplay/AwardPressDisplay';
 // import { createTheme, ThemeProvider } from '@mui/system';
 
 const AwardPressForm = (EditPageProps: {setAwardsPress: any;
@@ -35,11 +36,12 @@ const AwardPressForm = (EditPageProps: {setAwardsPress: any;
   return (
     <section>
       {EditPageProps.filmEPK.attributes !== undefined && <h2>{EditPageProps.filmEPK.attributes.movie_title}</h2>}
+      <AwardPressDisplay />
       <FormControl sx={{ m: 1, minWidth: 120 }}>
         <div className="press-awards-form" >
         <InputLabel id="award-or-press">type</InputLabel>
        <Select
-          style={{ width: '20%', minWidth: '85px', marginBottom: '3%'}}
+          style={{ width: '20%', minWidth: '85px', marginRight: '3%'}}
           labelId="award-or-press"
           id="award-or-press"
           // onChange={handleChange}
@@ -48,10 +50,10 @@ const AwardPressForm = (EditPageProps: {setAwardsPress: any;
           <MenuItem value={'award'}>award</MenuItem>
           <MenuItem value={'press'}>press</MenuItem>
         </Select>
-      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="publication" variant="outlined" />
-      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="link" variant="outlined" />
-      <TextField style={{ marginBottom: '3%'}} id="outlined-basic" label="quote" variant="outlined" />
-      <Button style={{ background: 'orange', height: '57px'}} variant="text">save</Button>
+      <TextField style={{ marginRight: '3%'}} id="outlined-basic" label="publication" variant="outlined" />
+      <TextField style={{ marginRight: '3%'}} id="outlined-basic" label="link" variant="outlined" />
+      <TextField style={{ marginRight: '3%'}} id="outlined-basic" label="quote" variant="outlined" />
+      <Button style={{ background: '#ec5f27', height: '57px'}} variant="text">save</Button>
       </div>
       </FormControl>
     </section>
