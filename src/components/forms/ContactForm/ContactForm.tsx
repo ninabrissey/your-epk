d3217dea2cfa2524db0ce0f5e2f54cffec7164ba
Maria DelSignore <maria.ad27@gmail.com>
2021-10-19 20:57:09 -0600

    Add attributes to inputs

--- src/components/forms/ContactForm/ContactForm.tsx
+++ src/components/forms/ContactForm/ContactForm.tsx
@@ -1,21 +1,75 @@
-import { useState, useEffect} from 'react'
+import { useState } from 'react'
 import { film, header } from "../../../utils/mockData";
+import { patchData } from '../../../utils/apiCalls';
 
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 
 const ContactForm = () => {
+  const [name, setName] = useState<string>('')
+  const [phoneNum, setPhoneNum] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [company, setCompany] = useState<string>('')
+
+  const handleSubmit = () => {
+    let currentContact = {
+      "user_id": 1,
+      "genre": name, 
+      "country": phoneNum, 
+      "website": email, 
+      "language": company
+    }
+    patchData(currentContact)
+    console.log('currentContact: ', currentContact)
+  }
 
   return (
     <section className='contact-form-container'>
       <h2>Contact</h2>
       <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-        <TextField id="outlined-basic" label="Company" variant="outlined" />
-        <Button variant="text">save</Button>
+        <TextField 
+          id="outlined-basic" 
+          label="Name" 
+          variant="outlined" 
+          type='text'
+          name='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {console.log('name: ', name)}
+        <TextField 
+          id="outlined-basic" 
+          label="Phone Number" 
+          variant="outlined" 
+          type='text'
+          name='phoneNum'
+          value={phoneNum}
+          onChange={(e) => setPhoneNum(e.target.value)}
+        />
+        <TextField 
+          id="outlined-basic" 
+          label="Email" 
+          variant="outlined" 
+          type='text'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField 
+          id="outlined-basic" 
+          label="Company" 
+          variant="outlined" 
+          type='text'
+          name='company'
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+        />
+        <Button 
+          variant="text"
+          onClick={handleSubmit}
+          >save
+        </Button>
       </FormControl>
     </section>
   )
