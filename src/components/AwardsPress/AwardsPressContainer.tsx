b7e1fa31d050b59f6ffcd21b8f7b42289037e3eb
Nina Brissey <nbrissey@gmail.com>
2021-10-21 16:45:48 -0600

    Build out container

--- src/components/AwardsPress/AwardsPressContainer.tsx
+++ src/components/AwardsPress/AwardsPressContainer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useState } from 'react'
+import { FilmEPK } from '../../types'
+import AwardPressDisplay from './AwardPressDisplay'
+import AwardPressForm from './AwardPressForm'
 
-const AwardsPressContainer = () => {
+const AwardsPressContainer = (filmProps: { filmEPK: FilmEPK }) => {
+const [isEditting, setIsEditting] = useState(false)
+
   return (
     <div>
-      
+      {isEditting&& <AwardPressForm {...filmProps}/>}
+    {!isEditting && <AwardPressDisplay />} 
     </div>
   )
 }
