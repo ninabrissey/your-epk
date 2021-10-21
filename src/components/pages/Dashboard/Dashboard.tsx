232189c45fd39e8b7306fcecdff5ca720ec4ffbd
Rachel Jensen <rachelangela@gmail.com>
2021-10-20 19:51:54 -0600

    Update dashboard display

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -6,48 +6,25 @@ import { ThisIsData, UserData } from '../../../types';
 import { setupMaster } from 'cluster';
 import { userInfo } from 'os';
 
-// this will be used with the network request most likely
-// interface DashBoardProps {
-//   data: ThisIsData
-// }
-
-
-// this will not live here
-const data = {
-  id: 1,
-  type: 'film_epk',
-  attributes: {
-    user_id: 1,
-    movie_title: 'Jimmies Cookies',
-    genre: 'horror',
-    country: 'vietnam',
-    release_year: 2013,
-    run_time: 223,
-    language: 'Vietnamese',
-    budget: 1000000,
-    website: 'http://www.example.com',
-    production_company: 'Universal Studios',
-    distribution: 'distribution'
-  }
-}
-
 const Dashboard = (dbprops: { id: number, name: string, setUser: any, user: UserData }) => {
 
   console.log('setUser method?', dbprops.user.first_name);
 
   useEffect(() => {
-    dbprops.setUser({
-      "email": "rachelmaria@gmail.com",
-      "first_name": "Nina",
-      "last_name": "Brissey"
-    })
+
+
+    // dbprops.setUser({
+    //   "email": "rachelmaria@gmail.com",
+    //   "first_name": "Nina",
+    //   "last_name": "Brissey"
+    // })
   }, [])
 
   return (
     <main>
       <h2>hey you</h2>
       <h3>{dbprops.user.email}</h3>
-      <TitleForm />
+      <TitleForm {...dbprops} />
       <EPKContainer />
     </main>
   )
