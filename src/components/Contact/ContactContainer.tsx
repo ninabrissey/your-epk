af942c0726c948a79e7d906e9f763d236de93e80
Maria DelSignore <maria.ad27@gmail.com>
2021-10-21 18:33:51 -0600

    Import form and display files

--- src/components/Contact/ContactContainer.tsx
+++ src/components/Contact/ContactContainer.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import ContactForm from './ContactForm';
+import ContactDisplay from './ContactDisplay';
+
+// do I need the container for contact or does the display live in the header display 
 
 const ContactContainer = () => {
   return (
-    <div>
-      
+    <div className='contact-container'>
+      {/* <ContactForm /> */}
+      <ContactDisplay />
     </div>
   )
 }
