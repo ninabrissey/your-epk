34b97483c0fe3549d071f6c1c1ba967d66d56ce0
Maria DelSignore <maria.ad27@gmail.com>
2021-10-21 19:06:18 -0600

    Add input field to return

--- src/components/Trailer/TrailerForm.tsx
+++ src/components/Trailer/TrailerForm.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+
 const TrailerForm = () => {
+  const [filmTrailer, setFilmTrailer] = useState<string>('')
+
+
+  const handleSubmit = () => {
+    let currentTrailer = {
+      trailer: filmTrailer
+    }
+  }
 
   return (
-    <p>trailer form</p>
+    <section className='trailer-container'>
+        <FormControl sx={{ m: 1, minWidth: 480 }}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Img URL"
+            type='text'
+            name='trailer'
+            value={filmTrailer}
+            onChange={(e) => setFilmTrailer(e.target.value)}
+          />
+          <Button 
+            variant="text"
+            onClick={handleSubmit}
+            >save
+          </Button>
+        </FormControl>
+    </section> 
   )
 }
 
