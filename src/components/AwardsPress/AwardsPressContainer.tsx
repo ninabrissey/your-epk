f8f192b3100abbd48307e681683b4f9722cc2642
Nina Brissey <nbrissey@gmail.com>
2021-10-21 21:34:51 -0600

    WIP

--- src/components/AwardsPress/AwardsPressContainer.tsx
+++ src/components/AwardsPress/AwardsPressContainer.tsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import { FilmEPK } from '../../types'
 import AwardPressDisplay from './AwardPressDisplay'
 import AwardPressForm from './AwardPressForm'
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
+import { film } from '../../utils/mockData';
 
-const AwardsPressContainer = (filmProps: { filmEPK: FilmEPK }) => {
+interface APContainerProps {
+  filmEPK: FilmEPK;
+  addFilmInfo: (filmInfo: object, id: number) => void
+}
+
+// const AwardsPressContainer = ({filmEPK}: APContainerProps) => {
+
+const AwardsPressContainer = ({filmEPK, addFilmInfo}: APContainerProps) => {
 const [isEditting, setIsEditting] = useState(false)
 
   return (
     <div>
-      {isEditting&& <AwardPressForm {...filmProps}/>}
-    {!isEditting && <AwardPressDisplay />} 
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      {(filmEPK.attributes.awards.length > 0 && filmEPK.attributes.press.length > 0) && 
+      <AwardPressDisplay press={filmEPK.attributes.press} awards={filmEPK.attributes.awards} />}
+      {isEditting && <AwardPressForm addFilmInfo={addFilmInfo} />}
     </div>
   )
 }
