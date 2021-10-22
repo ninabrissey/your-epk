cd6e812b205cdc657db1b5fd361b80d2c0bc5bf9
Maria DelSignore <maria.ad27@gmail.com>
2021-10-22 09:40:45 -0600

    Build component structure

---
+++ src/components/FilmPoster/FilmPosterDisplay.tsx
@@ -0,0 +1,19 @@
+interface IFilmPoster {
+  film
+}
+
+const FilmPosterDisplay = () => {
+
+  return (
+    <section className='film-poster-container'>
+      <p>I am the film poster display</p>
+      <img 
+        src=''
+        alt=''
+      />
+    </section>  
+  )
+}
+
+
+export default FilmPosterDisplay;
