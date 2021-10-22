88fdbd80ac0879f29c132ebe5fb83073182af47d
Rachel Jensen <rachelangela@gmail.com>
2021-10-22 17:00:58 -0600

    Add link press page from edit page

--- src/components/pages/EditPage/EditPage.tsx
+++ src/components/pages/EditPage/EditPage.tsx
@@ -1,7 +1,8 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { getUser, patchData } from '../../../utils/apiCalls';
-import { FilmEPK } from '../../../types'
-import "./EditPage.scss"
+import { FilmEPK, Attributes } from '../../../types';
+import { Link } from 'react-router-dom';
+import "./EditPage.scss";
 import AwardsPressContainer from '../../AwardsPress/AwardsPressContainer';
 import HeaderContainer from '../../Header/HeaderContainer';
 import TrailerContainer from '../../Trailer/TrailerContainer';
@@ -12,26 +13,33 @@ interface FilmProps {
 }
 
 const EditPage = ({ epk_id }: any) => {
-  const [film, setFilm] = useState<FilmEPK>({} as FilmEPK)
+  const [film, setFilm] = useState<FilmEPK>({} as FilmEPK);
+  const [title, setTitle] = useState('')
 
-// useEffect(() => {
-//   getUser(1)
-//     .then((data: any) => setFilm(data.included[0].attributes))
-//     // .then((data: any) => console.log(data.included[0].attributes))
-//     .catch(err => console.log(err))
-//     console.log(film)
-//   }, [])
-  
+  useEffect(() => {
+    getUser(1)
+      .then((data: any) => {
+        setFilm(data.included[0].attributes)
+        setTitle(data.included[0].attributes.movie_title)
+        console.log('film', data.included[0].attributes.movie_title)
+      })
+      // .then((data: any) => console.log(data.included[0].attributes))
+      .catch(err => console.log(err))
+  }, [])
+
   const addFilmInfo = (filmInfo: object) => {
     patchData(filmInfo, 77).then(data => setFilm(data))
   }
 
+
+
   return (
     <main className='edit-page'>
-      <HeaderContainer/>
-      <AwardsPressContainer filmEPK={film} addFilmInfo={addFilmInfo}/>
+      <HeaderContainer />
+      <AwardsPressContainer filmEPK={film} addFilmInfo={addFilmInfo} />
       <TrailerContainer />
       <FilmPosterContainer />
+      <Link to={`/${epk_id}/${title}`}>Go to Film Page</Link>
     </main>
   )
 }
