c062b15d7661cc784984b3610e4ef7080aa11524
Maria DelSignore <maria.ad27@gmail.com>
2021-10-21 20:04:25 -0600

    Add iframe element and interface

--- src/components/Trailer/TrailerDisplay.tsx
+++ src/components/Trailer/TrailerDisplay.tsx
@@ -1,7 +1,23 @@
-const TrailerDisplay = () => {
+import { useState, useEffect} from 'react'
+
+interface ITrailer {
+  trailer: string
+}
+
+const TrailerDisplay = ({trailerURL}: ITrailer) => {
 
   return (
-    <p>trailer display</p>
+    <div className='trailer-container'>
+      <iframe
+        className='trailer'
+        title='Embedded YouTube Video'
+        width='750'
+        height='438.46'
+        src={trailerURL.trailer}
+        frameBorder='0'
+        allowFullScreen
+      />
+    </div> 
   )
 }
 
