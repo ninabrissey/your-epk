9da6bd026656f481b9fe2d5fc0ea945fb167cf28
Maria DelSignore <maria.ad27@gmail.com>
2021-10-23 12:32:59 -0600

    Pass data and function through container

--- src/components/Synopsis/SynopsisContainer.tsx
+++ src/components/Synopsis/SynopsisContainer.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+import { FilmEPK } from '../../types';
 import SynopsisForm from '../Synopsis/SynopsisForm';
 import SynopsisDisplay from '../Synopsis/SynopsisDisplay';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
-const SynopsisContainer = () => {
+interface ISynopsis {
+  filmEPK: FilmEPK;
+  addFilmInfo: any;
+}
+
+const SynopsisContainer = ({ filmEPK, addFilmInfo } : any) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true)
+
   return (
     <div className='synopsis-container'>
-      <SynopsisForm />
-      {/* <SynopsisDisplay /> */}
+      <SynopsisForm filmEPK={filmEPK} addFilmInfo={addFilmInfo}/>
+      {/* <SynopsisDisplay filmEPK={filmEPK} addFilmInfo={addFilmInfo}/> */}
     </div>
   )
 }
