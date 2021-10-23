53088dc23db73a538a39db7baac7c0c1b327eb80
Rachel Jensen <rachelangela@gmail.com>
2021-10-23 13:59:51 -0600

    Add fetch data functionality

--- src/components/pages/PressPage/PressPage.tsx
+++ src/components/pages/PressPage/PressPage.tsx
@@ -1,8 +1,30 @@
+import HeaderDisplay from "../../Header/HeaderDisplay";
+import AwardPressDisplay from "../../AwardsPress/AwardPressDisplay";
+import { FilmEPK, EPKData } from '../../../types';
+import { useEffect, useState } from 'react';
+import { getEPK } from "../../../utils/apiCalls";
 
 
 const PressPage = ({ title, epk_id }: any) => {
+  const [epk, setEpk] = useState<FilmEPK>({} as FilmEPK)
 
-  return (<p>{`You've reached press page for ${title}, id# ${epk_id}`}</p>)
+  console.log(epk)
+
+  useEffect(() => {
+    getEPK(epk_id)
+      .then((info: EPKData) => {
+        setEpk(info.data)
+        console.log(epk)
+      })
+  }, [])
+
+  return (
+    <div>
+      {epk.id && <p>{`You've reached press page for ${epk.attributes.movie_title}, id# ${epk.attributes.synopsis}`}</p>}
+      <p>{` id# ${epk_id}`}</p>
+      {/* <HeaderDisplay /> */}
+    </div>
+  )
 }
 
 
