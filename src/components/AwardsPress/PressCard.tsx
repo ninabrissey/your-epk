a6499c9c78076d79eb48e0d4c2dc86b34f83af3e
Nina Brissey <nbrissey@gmail.com>
2021-10-22 23:34:19 -0600

    Remove mock data, utilize temp fetch

--- src/components/AwardsPress/PressCard.tsx
+++ src/components/AwardsPress/PressCard.tsx
@@ -5,17 +5,17 @@ interface IPressCard {
 }
 
 const PressCard = ({press} : IPressCard) => {
-  const presses =  [{
-    name_of_publication: "TIMES OF SAN DIEGO",
-    description: "Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him.",
-    link: "https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors"
-  }]
+  // const presses =  [{
+  //   name_of_publication: "TIMES OF SAN DIEGO",
+  //   description: "Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him.",
+  //   link: "https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors"
+  // }]
 
   return (
-    <article>
-      <a className="award-press-card" href={presses[0].link} target="_blank">
-        <h4>{presses[0].name_of_publication}</h4>
-        <h5>{presses[0].description}</h5>
+    <article className="award-press-display" style={{background: 'blue'}}>
+      <a className="award-press-card" href={press.link} target="_blank">
+        <h4>{press.name_of_publication}</h4>
+        <h5>{press.description}</h5>
       </a>
     </article>
   )
