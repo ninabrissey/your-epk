d15afbfdea97b7fd8abddd668eb921d6dc06b0bd
Nina Brissey <nbrissey@gmail.com>
2021-10-22 23:31:53 -0600

    Add conditionals to check if prop is undefined

--- src/components/AwardsPress/AwardPressDisplay.tsx
+++ src/components/AwardsPress/AwardPressDisplay.tsx
@@ -4,37 +4,30 @@ import AwardCard from './AwardCard';
 import './AwardPressDisplay.scss'
 
 interface APProps {
-  // presses: Press[],
+  presses: Press[],
   awards: Award[]
 }
 
-// const AwardPressDisplay = ({ presses, awards }: APProps) => {
-  const AwardPressDisplay = ({ awards }: APProps) => {
-  // const pressCards = presses.map(press => {
-  //   return <PressCard key={press.id} press={press}/>
-  // })
+const AwardPressDisplay = ({ awards, presses }: APProps) => {
+  
+  let pressCards; 
+  let awardCards;
 
-  const awardCards = awards.map(award => {
+  if (presses !== undefined) {
+    pressCards = presses.map(press => {
+    return <PressCard key={press.id} press={press}/>
+  })
+}
+  if (awards !== undefined) {
+    awardCards = awards.map(award => {
     return <AwardCard key={award.id} award={award}/>
   })
+}
 
   return (
     <section className="award-press-display">
-      {/* {pressCards} */}
+      {pressCards}
       {awardCards}
-      {/* {presses.length > 0 && <h2>{presses[0].description}</h2>}
-      <a className="award-press-card" href="https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors" target="_blank">
-        <h4>TIMES OF SAN DIEGO</h4>
-        <h5>"Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him."</h5>
-      </a>
-      <a className="award-press-card" href="https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors" target="_blank">
-        <h4>TIMES OF SAN DIEGO</h4>
-        <h5>"Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations."</h5>
-      </a>
-      <a className="award-press-card" href="https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors" target="_blank">
-        <h4>TIMES OF SAN DIEGO</h4>
-        <h5>"Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him."</h5>
-      </a> */}
     </section>
   )
 }
