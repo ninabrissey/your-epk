74079dc9317aadf58f698a32ef5497ab1a9622ad
Maria DelSignore <maria.ad27@gmail.com>
2021-10-23 12:33:12 -0600

    Add text field to form

--- src/components/Synopsis/SynopsisForm.tsx
+++ src/components/Synopsis/SynopsisForm.tsx
@@ -4,11 +4,35 @@ import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 
-const SynopsisForm = () => {
+const SynopsisForm = ({ addFilmInfo }: any) => {
+  const [synopsis, setSynopsis] = useState('')
+
+  const handleSubmit = () => {
+    addFilmInfo(synopsis, 133)
+    setSynopsis('')
+  }
 
   return (
     <form>
       <p>I am the synopsis form</p>
+      <FormControl sx={{ m: 1, minWidth: 320 }}>
+        <TextField
+          id='outlined-multiline-flexible'
+          label='Synopsis'
+          multiline
+          minRows={4}
+          maxRows={4}
+          type='text'
+          name='synopsis'
+          value={synopsis}
+          onChange={(e) => setSynopsis(e.target.value)}
+        />
+        <Button 
+          variant='text'
+          onClick={handleSubmit}
+          >save
+        </Button>
+      </FormControl>
     </form>
   )
 }
