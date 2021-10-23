ca8f6a499fd7f6a543d0bb606d543941b2646210
Nina Brissey <nbrissey@gmail.com>
2021-10-22 21:38:44 -0600

    Attempt to set state and access props that way - erroring

--- src/components/AwardsPress/AwardsPressContainer.tsx
+++ src/components/AwardsPress/AwardsPressContainer.tsx
@@ -1,38 +1,48 @@
-import { useState } from 'react'
-import { FilmEPK } from '../../types'
+import { useEffect, useState } from 'react'
+import { FilmEPK, Attributes, Award } from '../../types'
 import AwardPressDisplay from './AwardPressDisplay'
 import AwardPressForm from './AwardPressForm'
 import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
+import { film } from '../../utils/mockData';
 // import { film } from '../../utils/mockData';
 
 interface APContainerProps {
   filmEPK: FilmEPK;
-  addFilmInfo: any
-  // addFilmInfo:  void
+  addFilmInfo: any;
+  awards: Award[] | undefined;
 }
 
-// interface APContainerProps {
-//   filmEPK: FilmEPK;
-// }
-
-// const AwardsPressContainer = ({filmEPK}: APContainerProps) => {
-
 const AwardsPressContainer = ({filmEPK, addFilmInfo}: APContainerProps) => {
-// const AwardsPressContainer = ({filmEPK}: APContainerProps) => {
-const [isEditting, setIsEditting] = useState(true)
+const [isEditting, setIsEditting] = useState(false)
+const [awards, setAwards] = useState<Award[] | undefined >()
 
-  return (
-    <div>
-      <Fab color="secondary" aria-label="edit">
-        <EditIcon />
-      </Fab>
-      {/* {(filmEPK.attributes.awards.length > 0 && filmEPK.attributes.presses.length > 0) && 
-      <AwardPressDisplay presses={filmEPK.attributes.presses} awards={filmEPK.attributes.awards} />} */}
-      {isEditting && <AwardPressForm addFilmInfo={addFilmInfo} />}
-      {/* {isEditting && <AwardPressForm  />} */}
-    </div>
+console.log(filmEPK)
+
+useEffect(() => {
+  setAwards(filmEPK.attributes.awards)
+}, [film])
+
+return (
+  <div>
+    <Fab color="secondary" aria-label="edit">
+      <EditIcon />
+    </Fab>
+    {/* {awards !== undefined && 
+    <AwardPressDisplay presses={presses} awards={awards} />} */}
+     {awards !== undefined && 
+    <AwardPressDisplay awards={awards} />}
+    {isEditting && <AwardPressForm addFilmInfo={addFilmInfo} />}
+  </div>
   )
 }
 
 export default AwardsPressContainer
+
+// if (this.state.selectedMovie === null || undefined ) {
+//   return (
+//     <div>
+//       Loading
+//     </div>
+//   )
+// }
