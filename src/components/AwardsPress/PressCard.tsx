c7a5a3d38dc69c73ed38f75a34e53041f9131c18
Nina Brissey <nbrissey@gmail.com>
2021-10-22 23:35:02 -0600

    Remove mock data

--- src/components/AwardsPress/PressCard.tsx
+++ src/components/AwardsPress/PressCard.tsx
@@ -5,11 +5,6 @@ interface IPressCard {
 }
 
 const PressCard = ({press} : IPressCard) => {
-  // const presses =  [{
-  //   name_of_publication: "TIMES OF SAN DIEGO",
-  //   description: "Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him.",
-  //   link: "https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors"
-  // }]
 
   return (
     <article className="award-press-display" style={{background: 'blue'}}>
