429567cbde7172f97525bed421903d42a7005464
Maria DelSignore <maria.ad27@gmail.com>
2021-10-23 12:11:49 -0600

    Add imports on form component

---
+++ src/components/Synopsis/SynopsisForm.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+
+const SynopsisForm = () => {
+
+  return (
+    <form>
+      <p>I am the synopsis form</p>
+    </form>
+  )
+}
+
+
+export default SynopsisForm;
