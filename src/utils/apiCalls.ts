2f5af92a993b220678d52a5b1d5ad88eefdb9c6f
Rachel Jensen <rachelangela@gmail.com>
2021-10-22 19:49:18 -0600

    Add temporary findEPK function

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,3 +1,5 @@
+import { FilmEPK } from '../types'
+
 export const postData = (url: string, data: object) => {
   return fetch(url, {
     method: 'POST',
@@ -22,19 +24,24 @@ export const postData = (url: string, data: object) => {
 //   .catch(err => console.log('ERROR_IN_PATCH: ', err))
 // }
 
-export const patchData = (data : object, filmID : number) => {
+export const patchData = (data: object, filmID: number) => {
   return fetch(`https://epk-be.herokuapp.com/api/v1/film_epk/${filmID}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({film_epk: data})
+    body: JSON.stringify({ film_epk: data })
   })
-  .then(res => res.json())
+    .then(res => res.json())
 }
 
 
 export const getUser = (userID: number) => {
   return fetch(`https://epk-be.herokuapp.com/api/v1/users/${userID}`)
-  .then(res => res.json())
+    .then(res => res.json())
 }
+
+export const findEPK = (usersEPKS: FilmEPK[], epkID: string) => {
+  return usersEPKS.find(epk => epkID === epk.id)
+}
+
