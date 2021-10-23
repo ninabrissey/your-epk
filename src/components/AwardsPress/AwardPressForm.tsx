1c607016e528ae316fb1a7a6246e0ea4d46e82a1
Nina Brissey <nbrissey@gmail.com>
2021-10-22 23:32:51 -0600

    Add state

--- src/components/AwardsPress/AwardPressForm.tsx
+++ src/components/AwardsPress/AwardPressForm.tsx
@@ -14,20 +14,9 @@ const AwardPressForm = ({addFilmInfo}: any) => {
   const [publication, setPublication] = useState('');
   const [link, setLink] = useState('');
   const [quote, setQuote] = useState('');
-
-  // const newAward = {
-  //     awards: {
-  //       name: publication,
-  //       year: link,
-  //       award_type: quote
-  //     }
-  //   }
-
-    // const newAward = {
-    //     movie_title: publication,
-    //     genre: link,
-    //     country: quote
-    // }
+  const [awardName, setAwardName] = useState('')
+  const [awardType, setAwardType] = useState('')
+  const [awardYear, setAwardYear] = useState('')
   
     const newAward = {
         award: {
@@ -38,7 +27,7 @@ const AwardPressForm = ({addFilmInfo}: any) => {
   }
 
   // const newPress = {
-  //   presses: {
+  //   press: {
   //     publication: publication,
   //     link: link,
   //     quote: quote
@@ -66,7 +55,7 @@ const AwardPressForm = ({addFilmInfo}: any) => {
         <div className="press-awards-form" >
         <InputLabel id="award-or-press">type</InputLabel>
        <Select
-          style={{ width: '20%', minWidth: '85px', marginRight: '3%'}}
+          style={{ width: '20%', minWidth: '85px', marginRight: '3%', padding: 0}}
           labelId="award-or-press"
           id="award-or-press"
           label="type"
@@ -77,13 +66,27 @@ const AwardPressForm = ({addFilmInfo}: any) => {
           <MenuItem value={'award'}>award</MenuItem>
           <MenuItem value={'press'}>press</MenuItem>
         </Select>
-      <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="publication" label="publication" variant="outlined" value={publication} onChange={e => setPublication(e.target.value)}/>
-      <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="link" label="quote" variant="outlined" value={quote} onChange={e => setQuote(e.target.value)}/>
-      <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="quote" label="link" variant="outlined" value={link} onChange={e => setLink(e.target.value)}/>
-      {/* <Button style={{ background: '#ec5f27', height: '57px'}} variant="text"
-      onClick={() => addFilmInfo(newAward)}>save</Button> */}
-       <Button style={{ background: '#ec5f27', height: '57px'}} variant="text"
-      onClick={() => postData('https://epk-be.herokuapp.com/api/v1/awards', newAward)}>save</Button>
+
+        {select === 'award' && <div>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="year" label="year" variant="outlined" value={publication} onChange={e => setAwardYear(e.target.value)}/>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="name" label="name" variant="outlined" value={quote} onChange={e => setAwardName(e.target.value)}/>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="link" label="link" variant="outlined" value={link} onChange={e => setAwardType(e.target.value)}/>
+          {/* <Button style={{ background: '#ec5f27', height: '57px'}} variant="text"
+          onClick={() => addFilmInfo(newAward)}>save</Button> */}
+          <Button style={{ background: '#ec5f27', height: '57px'}} variant="text"
+          onClick={() => postData('https://epk-be.herokuapp.com/api/v1/awards', newAward)}>save</Button>
+        </div>}
+
+        {select === 'press' && <div>
+          <TextField style={{ margin: '1%', width: '150px'}} id="outlined-basic" name="publication" label="publication" variant="outlined" value={publication} size="small" onChange={e => setPublication(e.target.value)}/>
+          <TextField style={{ margin: '1%', width: '150px'}} id="outlined-basic" name="link" label="quote" variant="outlined" value={quote} size="small" onChange={e => setQuote(e.target.value)}/>
+          <TextField style={{ margin: '1%', width: '150px'}} id="outlined-basic" name="quote" label="link" variant="outlined" value={link} size="small" onChange={e => setLink(e.target.value)}/>
+          {/* <Button style={{ background: '#ec5f27', height: '57px'}} variant="text"
+          onClick={() => addFilmInfo(newAward)}>save</Button> */}
+          <Button style={{ background: '#ec5f27', margin: '1%'}} size="small"  variant="text"
+          onClick={() => postData('https://epk-be.herokuapp.com/api/v1/awards', newAward)}>save</Button>
+        </div>}
+
       </div>
       </FormControl>
     </section>
