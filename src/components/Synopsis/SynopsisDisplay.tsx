e24f680fd30a723d1863c21370138301ba349389
Maria DelSignore <maria.ad27@gmail.com>
2021-10-23 12:11:38 -0600

    Build display component

---
+++ src/components/Synopsis/SynopsisDisplay.tsx
@@ -0,0 +1,17 @@
+import { FilmEPK } from '../../types';
+
+interface ISynopsis {
+  filmEPK: FilmEPK;
+}
+
+const SynopsisDisplay = () => {
+
+  return (
+    <section>
+      <p>Synopsis renders here</p>
+    </section>
+  )
+}
+
+
+export default SynopsisDisplay;
