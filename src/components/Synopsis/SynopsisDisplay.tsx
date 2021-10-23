222fc87319109b31edd1e82b45b7a6325a536140
Maria DelSignore <maria.ad27@gmail.com>
2021-10-23 14:36:09 -0600

    Add conditional render to return

--- src/components/Synopsis/SynopsisDisplay.tsx
+++ src/components/Synopsis/SynopsisDisplay.tsx
@@ -1,14 +1,21 @@
-import { FilmEPK } from '../../types';
+import { useState, useEffect } from 'react';
+import { FilmEPK, Attributes } from '../../types';
 
 interface ISynopsis {
   filmEPK: FilmEPK;
 }
 
 const SynopsisDisplay = ({ filmEPK } : ISynopsis) => {
+  // const [currentEPK, setCurrentEPK] = useState<Attributes>({} as Attributes)
+
+  // useEffect(() => {
+  //   setCurrentEPK(filmEPK.attributes)
+  // }, [filmEPK])
 
   return (
     <section>
       <p>Synopsis renders here</p>
+      {filmEPK.attributes !== undefined && <p>{filmEPK.attributes.synopsis}</p>}
     </section>
   )
 }
