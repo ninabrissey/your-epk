3066b8fd8fd3b60a229421c7a289f6e0e15be5ec
Rachel Jensen <rachelangela@gmail.com>
2021-10-22 20:50:28 -0600

    Add simple navigation styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,15 @@
+import './Navigation.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../images/EPK_LOGO.png';
 
 const Navigation = ({ onEdit, epk_id, title }: any) => {
   return (
-    <nav>
-      <img src={logo} alt='Your EPK logo' />
-      <Link to={`/dashboard/1`}>Home</Link>
-      {onEdit && <Link to={`/${epk_id}/${title}`}>Press Kit</Link>}
+    <nav className='navigation'>
+      <img className='logo' src={logo} alt='Your EPK logo' />
+      <div className='menu'>
+        <Link to={`/dashboard/1`}>Home</Link>
+        {onEdit && <Link to={`/${epk_id}/${title}`}>Press Kit</Link>}
+      </div>
     </nav>
   );
 }
