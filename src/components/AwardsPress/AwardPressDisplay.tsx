457e5d1bc2437cbd3828d29e33ed021be9522859
Nina Brissey <nbrissey@gmail.com>
2021-10-22 21:37:39 -0600

    Add cards

--- src/components/AwardsPress/AwardPressDisplay.tsx
+++ src/components/AwardsPress/AwardPressDisplay.tsx
@@ -1,17 +1,28 @@
-import {useState} from 'react'
 import { FilmEPK, Press, Award } from '../../types';
+import PressCard from './PressCard';
+import AwardCard from './AwardCard';
 import './AwardPressDisplay.scss'
 
 interface APProps {
-  presses: Press[],
+  // presses: Press[],
   awards: Award[]
 }
 
-const AwardPressDisplay = ({presses, awards}: APProps) => {
+// const AwardPressDisplay = ({ presses, awards }: APProps) => {
+  const AwardPressDisplay = ({ awards }: APProps) => {
+  // const pressCards = presses.map(press => {
+  //   return <PressCard key={press.id} press={press}/>
+  // })
+
+  const awardCards = awards.map(award => {
+    return <AwardCard key={award.id} award={award}/>
+  })
 
   return (
     <section className="award-press-display">
-      {presses.length > 0 && <h2>{presses[0].description}</h2>}
+      {/* {pressCards} */}
+      {awardCards}
+      {/* {presses.length > 0 && <h2>{presses[0].description}</h2>}
       <a className="award-press-card" href="https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors" target="_blank">
         <h4>TIMES OF SAN DIEGO</h4>
         <h5>"Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him."</h5>
@@ -23,7 +34,7 @@ const AwardPressDisplay = ({presses, awards}: APProps) => {
       <a className="award-press-card" href="https://timesofsandiego.com/arts/2021/05/16/gi-film-fest-goes-virtual-with-largest-slate-ever-while-shining-light-on-women-warriors" target="_blank">
         <h4>TIMES OF SAN DIEGO</h4>
         <h5>"Amazing Grace,’ by first-time filmmaker Nina Brissey, leads with four nominations. The short tells the story of a Vietnam veteran dealing with alcoholism and PTSD and the daughter who struggles to care for him."</h5>
-      </a>
+      </a> */}
     </section>
   )
 }
