1d55acc23fa524430b36644220a64148a8ebb387
Rachel Jensen <rachelangela@gmail.com>
2021-10-23 17:46:11 -0600

    Add test conditions

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,2 +1,32 @@
 import getUser from '../fixtures/getUser.json';
 
+describe('Dashboard', () => {
+  beforeEach(() => {
+    cy.fixture('getUser.json').then((userInfo) => {
+      cy.intercept('https://epk-be.herokuapp.com/api/v1/users/1', userInfo)
+    })
+    
+    cy.visit('http://localhost:3000/dashboard/1')
+  })
+  
+  it('should do have a header with a logo', () => {
+    cy.get()
+  })
+
+  it('should do have a form for inputing a ', () => {
+
+  })
+
+
+  it('should display a user\'s existing EPKs', () => {
+    
+  })
+  
+  it('should allow a user to delete an EPK', () => {
+    
+  })
+
+  it('should be able to create a new EPK and redirect user to epk edit page', () => {
+
+  })
+})
