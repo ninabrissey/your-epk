3a2ff6b53ba8c190eba1908c6959e519e47cd208
Nina Brissey <nbrissey@gmail.com>
2021-10-24 03:49:21 -0600

    Create submitAndClear function and add required

--- src/components/AwardsPress/AwardPressForm.tsx
+++ src/components/AwardsPress/AwardPressForm.tsx
@@ -6,6 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import './AwardPressForm.scss'
+import PressCard from './PressCard';
 // import { createTheme, ThemeProvider } from '@mui/system';
 
 interface IAwardPressForm {
@@ -43,6 +44,18 @@ const AwardPressForm = ({addFilmInfo, postAwardsPress, setIsEditting, isEditting
     }
   }
 
+  const submitAndClear = (endpoint: string, newItem: any) => {
+    if ((newAward.award.name && newAward.award.year) || (newPress.press.publication && newPress.press.link)) {
+    setPublication('')
+    setLink('')
+    setQuote('')
+    setAwardName('') 
+    setAwardType('')
+    setAwardYear('')
+    postAwardsPress(endpoint, newItem)
+    }
+  }
+
   return (
     <section>
       <FormControl sx={{ m: 1, minWidth: 120 }}>
@@ -63,19 +76,19 @@ const AwardPressForm = ({addFilmInfo, postAwardsPress, setIsEditting, isEditting
         </Select>
 
         {select === 'award' && <>
-          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="name" label="name" variant="outlined" value={awardName} onChange={e => setAwardName(e.target.value)} helperText="name required" required />
-          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="year" label="year" variant="outlined" value={awardYear} onChange={e => setAwardYear(e.target.value)}/>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="name" label="name" variant="outlined" value={awardName} onChange={e => setAwardName(e.target.value)} helperText="name required" required/>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="year" label="year" variant="outlined" value={awardYear} onChange={e => setAwardYear(e.target.value)} helperText="year required" required/>
           <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="type" label="type" variant="outlined" value={awardType} onChange={e => setAwardType(e.target.value)}/>
           <Button style={{ background: '#ec5f27', height: '57px', marginRight: '3%'}} variant="text"
-          onClick={() => postAwardsPress('awards', newAward)}>save</Button>
+          onClick={() => submitAndClear('awards', newAward)}>save</Button>
         </>}
 
         {select === 'press' && <>
           <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="publication" label="publication" variant="outlined" value={publication} onChange={e => setPublication(e.target.value)} helperText="publication required" required />
-          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="quote" label="link" variant="outlined" value={link} onChange={e => setLink(e.target.value)} helperText="link required" required />
-          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="link" label="quote" variant="outlined" value={quote} onChange={e => setQuote(e.target.value)}/>
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="link" label="link" variant="outlined" value={link} onChange={e => setLink(e.target.value)} helperText="link required" required />
+          <TextField style={{ marginRight: '3%'}} id="outlined-basic" name="description" label="description/quote" variant="outlined" value={quote} onChange={e => setQuote(e.target.value)}/>
           <Button style={{ background: '#ec5f27', height:'57px', marginRight: '3%'}} variant="text"
-          onClick={() => postAwardsPress('presses', newPress)}>save</Button>
+          onClick={() => submitAndClear('presses', newPress)}>save</Button>
         </>}
 
         <Button style={{ background: '#ec5f27', height: '57px', width: '150px'}} variant="text"
