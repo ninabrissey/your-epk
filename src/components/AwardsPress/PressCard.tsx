178ad26e684a1e8e06d9a90a1d81983e76e9c5c4
Nina Brissey <nbrissey@gmail.com>
2021-10-24 01:14:48 -0600

    Change access of props due to change on BE

--- src/components/AwardsPress/PressCard.tsx
+++ src/components/AwardsPress/PressCard.tsx
@@ -1,7 +1,8 @@
 import { Press } from "../../types"
 
 interface IPressCard {
-  press: Press
+  // press: Press
+  press: any
 }
 
 const PressCard = ({press} : IPressCard) => {
@@ -9,8 +10,8 @@ const PressCard = ({press} : IPressCard) => {
   return (
     <article className="award-press-display" style={{background: 'blue'}}>
       <a className="award-press-card" href={press.link} target="_blank">
-        <h4>{press.name_of_publication}</h4>
-        <h5>{press.description}</h5>
+        <h4>{press.attributes.name_of_publication}</h4>
+        <h5>{press.attributes.description}</h5>
       </a>
     </article>
   )
