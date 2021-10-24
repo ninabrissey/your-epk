d05c06776b0dd7f472b55cb3fe24552a63855e4c
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 14:03:02 -0600

    Preitter did the thing

--- src/components/pages/EditPage/EditPage.tsx
+++ src/components/pages/EditPage/EditPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { patchData, getEPK } from '../../../utils/apiCalls';
 import { FilmEPK, EPKData, Award, Press } from '../../../types';
 import AwardsPressContainer from '../../AwardsPress/AwardsPressContainer';
@@ -7,7 +7,7 @@ import TrailerContainer from '../../Trailer/TrailerContainer';
 import FilmPosterContainer from '../../FilmPoster/FilmPosterContainer';
 import Navigation from '../../Navigation/Navigation';
 import { filterIncluded } from '../../../utils/cleanData';
-import "./EditPage.scss"
+import './EditPage.scss';
 import SynopsisContainer from '../../Synopsis/SynopsisContainer';
 import FilmDetailsContainer from '../../FilmDetails/FilmDetailsContainer';
 import TaglinesContainer from '../../Taglines/TaglinesContainer';
@@ -17,50 +17,54 @@ import TaglinesContainer from '../../Taglines/TaglinesContainer';
 // }
 
 const EditPage = ({ epk_id }: any) => {
-  const [film, setFilm] = useState<FilmEPK>({} as FilmEPK);
-  const [title, setTitle] = useState('')
-  const [awards, setAwards] = useState<Array<Award>>([]);
-  const [presses, setPress] = useState<Array<Press>>([])
+	const [film, setFilm] = useState<FilmEPK>({} as FilmEPK);
+	const [title, setTitle] = useState('');
+	const [awards, setAwards] = useState<Array<Award>>([]);
+	const [presses, setPress] = useState<Array<Press>>([]);
 
-  useEffect(() => {
-    getEPK(epk_id)
-      .then((data: EPKData) => {
-        setFilm(data.data)
-        setTitle(formatTitle(data.data.attributes.movie_title))
-        setAwards(filterIncluded(data.included, 'award'))
-        // function will need to filter "awards", "press", "film_fam", "images"
-      })
-      .catch(err => console.log(err))
-  }, [])
+	useEffect(() => {
+		getEPK(epk_id)
+			.then((data: EPKData) => {
+				setFilm(data.data);
+				setTitle(formatTitle(data.data.attributes.movie_title));
+				setAwards(filterIncluded(data.included, 'award'));
+				// function will need to filter "awards", "press", "film_fam", "images"
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
+	// console.log('filmEPK in editPage: ', film)
 
-  // console.log('filmEPK in editPage: ', film)
+	const addFilmInfo = (filmInfo: object) => {
+		patchData(filmInfo, 137).then((data) => setFilm(data.data));
+	};
 
-  const addFilmInfo = (filmInfo: object) => {
-    patchData(filmInfo, 133).then(data => setFilm(data.data))
-  }
+	const formatTitle = (title: string) => {
+		return title.split(' ').join('-');
+	};
 
-  const formatTitle = (title: string) => {
-    return title.split(' ').join('-')
-  }
-
-  return (
-    <div>
-      <Navigation onEdit={true} epk_id={epk_id} title={title} />
-      <main className='edit-page'>
-        <HeaderContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-        {/* <AwardsPressContainer filmEPK={film} addFilmInfo={addFilmInfo} /> */}
-        <AwardsPressContainer awards={awards} presses={presses} addFilmInfo={addFilmInfo} epk_id={epk_id} />
-        <TrailerContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-        <div className='container-wrapper'>
-          <SynopsisContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-          <FilmPosterContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-        </div>
-        <FilmDetailsContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-        <TaglinesContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-      </main>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navigation onEdit={true} epk_id={epk_id} title={title} />
+			<main className="edit-page">
+				<HeaderContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+				{/* <AwardsPressContainer filmEPK={film} addFilmInfo={addFilmInfo} /> */}
+				<AwardsPressContainer
+					awards={awards}
+					presses={presses}
+					addFilmInfo={addFilmInfo}
+					epk_id={epk_id}
+				/>
+				<TrailerContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+				<div className="container-wrapper">
+					<SynopsisContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+					<FilmPosterContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+				</div>
+				<FilmDetailsContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+				<TaglinesContainer filmEPK={film} addFilmInfo={addFilmInfo} />
+			</main>
+		</div>
+	);
+};
 
 export default EditPage;
