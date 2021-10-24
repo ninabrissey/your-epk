754950b1a8631eaf77ffaec5155b828b8fc5f73c
Nina Brissey <nbrissey@gmail.com>
2021-10-24 04:56:49 -0600

    Add classNames and styling

--- src/components/AwardsPress/AwardCard.tsx
+++ src/components/AwardsPress/AwardCard.tsx
@@ -1,17 +1,25 @@
 import { Award } from "../../types"
+import laurel from './../../images/laurel.png'
+import './AwardPress.scss'
 
 interface IAwardCard {
   // award: Award
-  award: any
+  award: any,
+  style: any
 }
 
-const AwardCard = ({ award } : IAwardCard) => {
+const AwardCard = ({ award, style } : IAwardCard) => {
 
   return (
-    <article className="award-press-card">
-      <p>{award.attributes.name}</p>
-      {award.attributes.award_type && <p>{award.attributes.award_type}</p>}
-      <p>{award.attributes.year}</p>
+    <article style={style} className="award-press-card">
+      <div className="award-card-styling-div">
+        <img className="laurel" src={laurel} alt="laurels" />
+        <div className="award-text">
+          <p>{award.attributes.name}</p>
+          {award.attributes.award_type && <p>{award.attributes.award_type}</p>}
+          <p>{award.attributes.year}</p>
+        </div>
+      </div>
     </article>
   )
 }
