17ca4d9c5ee4c5d02a636b3b7fb6bb9eba691b14
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 11:19:16 -0600

    Builds base component structure

---
+++ src/components/Taglines/TaglinesForm.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+
+const TaglinesForm = () => {
+
+  return (
+    <form>
+      <p>I am the taglines form</p>
+    </form>
+  )
+}
+
+
+export default TaglinesForm;
