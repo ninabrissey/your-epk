a4f86448911e71349b8317d901fb28f97500154b
Nina Brissey <nbrissey@gmail.com>
2021-10-24 17:57:04 -0600

    Add state for images

--- src/components/pages/EditPage/EditPage.tsx
+++ src/components/pages/EditPage/EditPage.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { patchData, getEPK } from '../../../utils/apiCalls';
-import { FilmEPK, EPKData, Award, Press } from '../../../types';
+import { FilmEPK, EPKData, Award, Press, Image } from '../../../types';
+import { filterIncluded } from '../../../utils/cleanData';
 import AwardsPressContainer from '../../AwardsPress/AwardsPressContainer';
 import HeaderContainer from '../../Header/HeaderContainer';
 import TrailerContainer from '../../Trailer/TrailerContainer';
 import FilmPosterContainer from '../../FilmPoster/FilmPosterContainer';
 import Navigation from '../../Navigation/Navigation';
-import { filterIncluded } from '../../../utils/cleanData';
-import "./EditPage.scss"
 import SynopsisContainer from '../../Synopsis/SynopsisContainer';
 import FilmDetailsContainer from '../../FilmDetails/FilmDetailsContainer';
 import TaglinesContainer from '../../Taglines/TaglinesContainer';
+import './EditPage.scss';
+import ImagesContainer from '../../Images/ImagesContainer';
 
 // interface FilmProps {
 //   filmEPK: FilmEPK;
@@ -18,49 +19,55 @@ import TaglinesContainer from '../../Taglines/TaglinesContainer';
 
 const EditPage = ({ epk_id }: any) => {
   const [film, setFilm] = useState<FilmEPK>({} as FilmEPK);
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('');
   const [awards, setAwards] = useState<Array<Award>>([]);
-  const [presses, setPress] = useState<Array<Press>>([])
+  const [presses, setPress] = useState<Array<Press>>([]);
+  const [images, setImages] = useState<Image[] | []>([]);
 
   useEffect(() => {
     getEPK(epk_id)
       .then((data: EPKData) => {
-        setFilm(data.data)
-        setTitle(formatTitle(data.data.attributes.movie_title))
-        setAwards(filterIncluded(data.included, 'award'))
+        setFilm(data.data);
+        setTitle(formatTitle(data.data.attributes.movie_title));
+        setAwards(filterIncluded(data.included, 'award'));
         // function will need to filter "awards", "press", "film_fam", "images"
       })
-      .catch(err => console.log(err))
-  }, [])
-
+      .catch((err) => console.log(err));
+  }, []);
 
   // console.log('filmEPK in editPage: ', film)
 
   const addFilmInfo = (filmInfo: object) => {
-    patchData(filmInfo, 133).then(data => setFilm(data.data))
-  }
+    patchData(filmInfo, 133).then((data) => setFilm(data.data));
+  };
 
   const formatTitle = (title: string) => {
-    return title.split(' ').join('-')
-  }
+    return title.split(' ').join('-');
+  };
 
   return (
     <div>
       <Navigation onEdit={true} epk_id={epk_id} title={title} />
-      <main className='edit-page'>
+      <main className="edit-page">
         <HeaderContainer filmEPK={film} addFilmInfo={addFilmInfo} />
         {/* <AwardsPressContainer filmEPK={film} addFilmInfo={addFilmInfo} /> */}
-        <AwardsPressContainer awards={awards} presses={presses} addFilmInfo={addFilmInfo} epk_id={epk_id} />
+        <AwardsPressContainer
+          awards={awards}
+          presses={presses}
+          addFilmInfo={addFilmInfo}
+          epk_id={epk_id}
+        />
         <TrailerContainer filmEPK={film} addFilmInfo={addFilmInfo} />
-        <div className='container-wrapper'>
+        <div className="container-wrapper">
           <SynopsisContainer filmEPK={film} addFilmInfo={addFilmInfo} />
           <FilmPosterContainer filmEPK={film} addFilmInfo={addFilmInfo} />
         </div>
+        {/* <ImagesContainer epk_id={epk_id} images={images} /> */}
         <FilmDetailsContainer filmEPK={film} addFilmInfo={addFilmInfo} />
         <TaglinesContainer filmEPK={film} addFilmInfo={addFilmInfo} />
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default EditPage;
