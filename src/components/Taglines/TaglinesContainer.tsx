9726f18f9da47118b9392a13aa09dac0e9691bad
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 11:17:04 -0600

    Add imports for display and form

--- src/components/Taglines/TaglinesContainer.tsx
+++ src/components/Taglines/TaglinesContainer.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from 'react';
+import { FilmEPK } from '../../types';
+import TaglinesForm from '../Taglines/TaglinesForm';
+import TaglinesDisplay from '../Taglines/TaglinesDisplay';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
 const TaglinesContainer = () => {
+  const [isEditing, setIsEditing] = useState<boolean>(true)
+
   return (
     <div>
-      
+      <TaglinesForm />
+      <TaglinesDisplay />
     </div>
   )
 }
