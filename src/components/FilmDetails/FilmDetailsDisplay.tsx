a7f9b986e791f36dca7ea08eae6a15e8565920c5
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 09:06:50 -0600

    Add jsx tags to render film details

--- src/components/FilmDetails/FilmDetailsDisplay.tsx
+++ src/components/FilmDetails/FilmDetailsDisplay.tsx
@@ -8,7 +8,18 @@ const FilmDetailsDisplay = ({ filmEPK } : IFilmDetails) => {
 
   return (
     <section>
-      <p>film deets render here</p>
+      <h2>Film Details</h2>
+      {filmEPK.attributes !== undefined && <div>
+        <p>{filmEPK.attributes.genre}</p>
+        <p>{filmEPK.attributes.country}</p>
+        <p>{filmEPK.attributes.release_year}</p>
+        <p>{filmEPK.attributes.run_time}</p>
+        <p>{filmEPK.attributes.language}</p>
+        <p>{filmEPK.attributes.budget}</p>
+        <p>{filmEPK.attributes.production_company}</p>
+        <p>{filmEPK.attributes.website}</p>
+      </div>}
+      {console.log('filmEPK in FILM DETAILS DISPLAY', filmEPK)}
     </section>
   )
 }
