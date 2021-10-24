73862fa08cdc4dc2cda3fb21e8f5442974499c03
Nina Brissey <nbrissey@gmail.com>
2021-10-24 03:48:21 -0600

    Add conditional rendering

--- src/components/AwardsPress/AwardCard.tsx
+++ src/components/AwardsPress/AwardCard.tsx
@@ -6,17 +6,12 @@ interface IAwardCard {
 }
 
 const AwardCard = ({ award } : IAwardCard) => {
-  const awards =  [{
-    name: "The Super Award",
-    year: "1999",
-    award_type: "Shiny"
-  }]
 
   return (
     <article className="award-press-card">
-      <p>{award.attributes.year}</p>
-      <p>{award.attributes.award_type}</p>
       <p>{award.attributes.name}</p>
+      {award.attributes.award_type && <p>{award.attributes.award_type}</p>}
+      <p>{award.attributes.year}</p>
     </article>
   )
 }
