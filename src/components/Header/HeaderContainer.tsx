b93a61385574e3e1c44355a78bafdd8e471ea075
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 13:40:12 -0600

    Remove unused code

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { FilmEPK } from '../../types';
 import HeaderForm from './HeaderForm';
 import HeaderDisplay from './HeaderDisplay';
@@ -6,26 +6,24 @@ import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
 
 interface IHeader {
-  filmEPK: FilmEPK;
-  addFilmInfo: any;
+	filmEPK: FilmEPK;
+	addFilmInfo: any;
 }
 
-const HeaderContainer = ({ filmEPK, addFilmInfo } : IHeader) => {
-  const [isEditing, setIsEditing] = useState<boolean>(true)
+const HeaderContainer = ({ filmEPK, addFilmInfo }: IHeader) => {
+	const [isEditing, setIsEditing] = useState<boolean>(true);
 
-  // console.log('filmEPK in Header Container: ', filmEPK)
-
-  return (
-    <div className='header-container'>
-      {!isEditing && 
-        <Fab color='secondary' aria-label='edit'>
-          <EditIcon />
-        </Fab>
-      }
-      {isEditing && <HeaderForm filmEPK={filmEPK} addFilmInfo={addFilmInfo} />}
-      {!isEditing && <HeaderDisplay filmEPK={filmEPK} />}
-    </div>
-  )
-}
+	return (
+		<div className="header-container">
+			{!isEditing && (
+				<Fab color="secondary" aria-label="edit">
+					<EditIcon />
+				</Fab>
+			)}
+			{isEditing && <HeaderForm filmEPK={filmEPK} addFilmInfo={addFilmInfo} />}
+			{!isEditing && <HeaderDisplay filmEPK={filmEPK} />}
+		</div>
+	);
+};
 
 export default HeaderContainer;
