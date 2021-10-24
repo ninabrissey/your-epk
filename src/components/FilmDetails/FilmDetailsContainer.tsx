c29deca449d1daea4da380f15368d543d5fe635b
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 00:10:02 -0600

    Import display and form into component

--- src/components/FilmDetails/FilmDetailsContainer.tsx
+++ src/components/FilmDetails/FilmDetailsContainer.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import { FilmEPK } from '../../types';
+import FilmDetailsForm from './FilmDetailsForm';
+import FilmDetailsDisplay from './FilmDetailsDisplay';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
-const FilmDetailsContainer = () => {
+interface IFilmDetails {
+  filmEPK: FilmEPK;
+  addFilmInfo: any;
+}
+
+
+const FilmDetailsContainer = ({ filmEPK, addFilmInfo } : IFilmDetails ) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true)
+
   return (
-    <div>
-      
-    </div>
+    <section>
+      {isEditing && <FilmDetailsForm 
+        filmEPK={filmEPK} 
+        addFilmInfo={addFilmInfo} 
+      />}
+      {!isEditing && <FilmDetailsDisplay filmEPK={filmEPK}  />}
+    </section>
   )
 }
 
