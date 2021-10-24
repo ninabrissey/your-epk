acb1dcbb684e24a7c27b0ff15bb51999da30edf7
Rachel Jensen <rachelangela@gmail.com>
2021-10-23 21:05:03 -0600

    Add initial dashboard tests

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -9,24 +9,35 @@ describe('Dashboard', () => {
     cy.visit('http://localhost:3000/dashboard/1')
   })
   
-  it('should do have a header with a logo', () => {
-    cy.get()
+  it('should do have a header with a logo and no home link', () => {
+    cy.get('.logo').should('be.visible')
+      .get('.menu').should('not.exist')
   })
-
-  it('should do have a form for inputing a ', () => {
-
-  })
-
 
   it('should display a user\'s existing EPKs', () => {
+    cy.get('.titles-container').children().should('have.length', 2)
+    cy.get(':nth-child(1) > .linked-title > .go-to-edit > .title-text').contains('The Adventures of Mini')
     
+  })
+
+  it('should do have a form for inputing a new EPK', () => {
+    cy.get('.create-epk').should('not.exist')
+      .get('[type="text"]').type('Awesome Movie')
+      .get('.create-epk').should('be.visible').click()
+    
+    cy.get('.titles-container').children().should('have.length', 3)
+
+    cy.get(':nth-child(1) > .linked-title > .go-to-edit > .title-text').contains('Awesome Movie')
   })
   
   it('should allow a user to delete an EPK', () => {
+    cy.intercept('https://epk-be.herokuapp.com/api/v1/film_epk/1', {
+      status: 204
+    })
     
+    cy.get('.titles-container > :nth-child(1) > .MuiButton-root').click()
+
+    cy.get(':nth-child(1) > .linked-title > .go-to-edit > .title-text').contains('Film That\'s Bueno')
   })
 
-  it('should be able to create a new EPK and redirect user to epk edit page', () => {
-
-  })
 })
