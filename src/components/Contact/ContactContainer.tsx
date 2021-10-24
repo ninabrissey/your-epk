2adb7c0c46baaecfa53fe37f3a04c80a3667140d
Maria DelSignore <maria.ad27@gmail.com>
2021-10-24 16:05:09 -0600

    Add container for contact form and display

---
+++ src/components/Contact/ContactContainer.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { FilmEPK } from '../../types';
+import ContactDisplay from './ContactDisplay';
+import ContactForm from './ContactForm';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
+
+interface IContact {
+	filmEPK: FilmEPK;
+	addFilmInfo: any;
+}
+
+const ContactContainer = ({ filmEPK, addFilmInfo }: IContact) => {
+	const [isEditing, setIsEditing] = useState<string>('');
+
+	return (
+		<section>
+			{!isEditing && (
+				<Fab color="secondary" aria-label="edit">
+					<EditIcon />
+				</Fab>
+			)}
+			{isEditing && (
+				<ContactForm addFilmInfo={addFilmInfo} setIsEditing={setIsEditing} />
+			)}
+			{!isEditing && <ContactDisplay filmEPK={filmEPK} />}
+		</section>
+	);
+};
+
+export default ContactContainer;
