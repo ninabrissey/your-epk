d0a7de356730e3961eb7a785fa8b1e9c28e4f530
Nina Brissey <nbrissey@gmail.com>
2021-10-25 17:44:41 -0600

    Attempt to keep fields populated

--- src/components/Synopsis/SynopsisForm.tsx
+++ src/components/Synopsis/SynopsisForm.tsx
@@ -5,38 +5,38 @@ import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 
 const SynopsisForm = ({ addFilmInfo, setIsEditing }: any) => {
-	const [synopsis, setSynopsis] = useState('');
+  const [synopsis, setSynopsis] = useState('');
 
-	const handleSubmit = () => {
-		const currentSynopsis = {
-			synopsis: synopsis,
-		};
-		addFilmInfo(currentSynopsis, 133);
-		setIsEditing(false);
-		setSynopsis('');
-	};
+  const handleSubmit = () => {
+    const currentSynopsis = {
+      synopsis: synopsis,
+    };
+    addFilmInfo(currentSynopsis, 133);
+    setIsEditing(false);
+    setSynopsis('');
+  };
 
-	return (
-		<form>
-			<p>I am the synopsis form</p>
-			<FormControl sx={{ m: 1, minWidth: 320 }}>
-				<TextField
-					id="outlined-multiline-flexible"
-					label="Synopsis"
-					multiline
-					minRows={4}
-					maxRows={4}
-					type="text"
-					name="synopsis"
-					value={synopsis}
-					onChange={(e) => setSynopsis(e.target.value)}
-				/>
-				<Button variant="text" onClick={handleSubmit}>
-					save
-				</Button>
-			</FormControl>
-		</form>
-	);
+  return (
+    <form>
+      <p>I am the synopsis form</p>
+      <FormControl sx={{ m: 1, minWidth: 320 }}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Synopsis"
+          multiline
+          minRows={4}
+          maxRows={4}
+          type="text"
+          name="synopsis"
+          value={synopsis}
+          onChange={(e) => setSynopsis(e.target.value)}
+        />
+        <Button variant="text" onClick={handleSubmit}>
+          save
+        </Button>
+      </FormControl>
+    </form>
+  );
 };
 
 export default SynopsisForm;
