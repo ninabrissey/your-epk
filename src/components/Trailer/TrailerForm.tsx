90715c855885e4c1a151dc7827f9dbb82ff240d7
Maria DelSignore <maria.ad27@gmail.com>
2021-10-25 15:41:58 -0600

    Add functionality to toggle trailer views

--- src/components/Trailer/TrailerForm.tsx
+++ src/components/Trailer/TrailerForm.tsx
@@ -4,39 +4,36 @@ import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 
-const TrailerForm = ({ addFilmInfo } : any) => {
-  const [filmTrailer, setFilmTrailer] = useState<string>('')
+const TrailerForm = ({ addFilmInfo, setIsEditing }: any) => {
+	const [filmTrailer, setFilmTrailer] = useState<string>('');
 
+	const handleSubmit = () => {
+		let currentTrailer = {
+			trailer: filmTrailer,
+		};
+		addFilmInfo(currentTrailer);
+		setIsEditing(false);
+		setFilmTrailer('');
+	};
 
-  const handleSubmit = () => {
-    let currentTrailer = {
-      trailer: filmTrailer
-    }
-    addFilmInfo(currentTrailer)
-    setFilmTrailer('')
-  }
-
-  return (
-    <form>
-        <p>I am the trailer form container</p>
-        <FormControl sx={{ m: 1, minWidth: 480 }}>
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Img URL"
-            type='text'
-            name='trailer'
-            value={filmTrailer}
-            onChange={(e) => setFilmTrailer(e.target.value)}
-          />
-          <Button 
-            variant="text"
-            onClick={handleSubmit}
-            >save
-          </Button>
-        </FormControl>
-    </form> 
-  )
-}
-
+	return (
+		<form>
+			<p>I am the trailer form container</p>
+			<FormControl sx={{ m: 1, minWidth: 480 }}>
+				<TextField
+					id="outlined-multiline-flexible"
+					label="Img URL"
+					type="text"
+					name="trailer"
+					value={filmTrailer}
+					onChange={(e) => setFilmTrailer(e.target.value)}
+				/>
+				<Button variant="text" onClick={handleSubmit}>
+					save
+				</Button>
+			</FormControl>
+		</form>
+	);
+};
 
 export default TrailerForm;
