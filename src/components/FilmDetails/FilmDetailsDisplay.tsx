6fc93b5d5b22423dc4fed2ab1d3776ed063233dd
Nina Brissey <nbrissey@gmail.com>
2021-10-26 10:07:00 -0600

    Add classNames and details categories

--- src/components/FilmDetails/FilmDetailsDisplay.tsx
+++ src/components/FilmDetails/FilmDetailsDisplay.tsx
@@ -4,24 +4,25 @@ interface IFilmDetails {
   filmEPK: FilmEPK;
 }
 
-const FilmDetailsDisplay = ({ filmEPK } : IFilmDetails) => {
-
+// Maria, I may have added c;asses here - take a look at the two classNames and scss
+const FilmDetailsDisplay = ({ filmEPK }: IFilmDetails) => {
   return (
-    <section>
+    <section className="film-details-section">
       <h2>Film Details</h2>
-      {filmEPK.attributes !== undefined && <div>
-        <p>{filmEPK.attributes.genre}</p>
-        <p>{filmEPK.attributes.country}</p>
-        <p>{filmEPK.attributes.release_year}</p>
-        <p>{filmEPK.attributes.run_time}</p>
-        <p>{filmEPK.attributes.language}</p>
-        <p>{filmEPK.attributes.budget}</p>
-        <p>{filmEPK.attributes.production_company}</p>
-        <p>{filmEPK.attributes.website}</p>
-      </div>}
+      {filmEPK.attributes !== undefined && (
+        <div className="film-details-text">
+          <p>Genre: {filmEPK.attributes.genre}</p>
+          <p>Country of origin: {filmEPK.attributes.country}</p>
+          <p>Realeased: {filmEPK.attributes.release_year}</p>
+          <p>Runtime: {filmEPK.attributes.run_time}</p>
+          <p>Langauge: {filmEPK.attributes.language}</p>
+          <p>Budget: {filmEPK.attributes.budget}</p>
+          <p>Production Company: {filmEPK.attributes.production_company}</p>
+          <p>Website: {filmEPK.attributes.website}</p>
+        </div>
+      )}
     </section>
-  )
-}
-
+  );
+};
 
 export default FilmDetailsDisplay;
