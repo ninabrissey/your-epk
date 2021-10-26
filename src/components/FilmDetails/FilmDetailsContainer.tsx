7e16014970c58c750e07898b71d5d6f077c183ac
Nina Brissey <nbrissey@gmail.com>
2021-10-25 21:12:26 -0600

    Add comments regarding loading happening on the details container

--- src/components/FilmDetails/FilmDetailsContainer.tsx
+++ src/components/FilmDetails/FilmDetailsContainer.tsx
@@ -10,35 +10,36 @@ interface IFilmDetails {
   addFilmInfo: any;
 }
 
+const FilmDetailsContainer = ({ filmEPK, addFilmInfo }: IFilmDetails) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true);
 
-const FilmDetailsContainer = ({ filmEPK, addFilmInfo } : IFilmDetails ) => {
-  const [isEditing, setIsEditing] = useState<boolean>(true)
+  // Nina Note
+  // I believe that error handling and loading is all being handled
+  // Edit page for everything except for the arrays
 
   return (
-    <section className='film-details-container'>
-      
-      {!isEditing && 
-        <Fab 
-          color='secondary' 
-          aria-label='edit'
+    <section className="film-details-container">
+      {!isEditing && (
+        <Fab
+          color="secondary"
+          aria-label="edit"
           onClick={() => setIsEditing(true)}
         >
           <EditIcon />
         </Fab>
-      }
+      )}
 
-      {isEditing && <FilmDetailsForm 
-        filmEPK={filmEPK} 
-        addFilmInfo={addFilmInfo} 
-        setIsEditing={setIsEditing}
-      />}
+      {isEditing && (
+        <FilmDetailsForm
+          filmEPK={filmEPK}
+          addFilmInfo={addFilmInfo}
+          setIsEditing={setIsEditing}
+        />
+      )}
 
-      {!isEditing && <FilmDetailsDisplay 
-        filmEPK={filmEPK} 
-      />}
-
+      {!isEditing && <FilmDetailsDisplay filmEPK={filmEPK} />}
     </section>
-  )
-}
+  );
+};
 
-export default FilmDetailsContainer
+export default FilmDetailsContainer;
