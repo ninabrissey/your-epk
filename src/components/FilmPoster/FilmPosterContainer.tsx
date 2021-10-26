4f9b3c1a4d1252dc351dbfeafbbb4732fe524d6a
Rachel Jensen <rachelangela@gmail.com>
2021-10-26 14:29:00 -0600

    Add poster and editing to display props

--- src/components/FilmPoster/FilmPosterContainer.tsx
+++ src/components/FilmPoster/FilmPosterContainer.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FilmEPK } from '../../types';
 import FilmPosterForm from '../FilmPoster/FilmPosterForm';
 import FilmPosterDisplay from '../FilmPoster/FilmPosterDisplay';
@@ -8,24 +8,33 @@ import EditIcon from '@mui/icons-material/Edit';
 interface IFilmPoster {
 	filmEPK: FilmEPK;
 	addFilmInfo: any;
+	loading: boolean
 }
 
 const FilmPosterContainer = ({ filmEPK, addFilmInfo }: IFilmPoster) => {
   const [isEditing, setIsEditing] = useState<boolean>(true)
   const [poster, setPoster] = useState<string>('')
+	const [hasEPK, setHasEPK] = useState<boolean>(false)
 
-  console.log(poster)
+	// useEffect(() => {
+	// 	setTimeout(() => {setPoster(filmEPK.attributes.movie_poster_url)}, 10000)
+	
+
+	// }, [])
 
   return (
-    <div className='film-poster-container'>
-      {!isEditing &&
-        <Fab color='secondary' aria-label='edit' onClick={() => setIsEditing(true)}>
-          <EditIcon />
-        </Fab>
-      }
-      {isEditing && <FilmPosterForm filmEPK={filmEPK} addFilmInfo={addFilmInfo} setPoster={setPoster} />}
-      {!isEditing && <FilmPosterDisplay filmEPK={filmEPK} />}
-    </div>
+		<div className='film-poster-container'> 
+		{ !filmEPK.attributes ? <p>loading</p> :
+			<div >
+				{!isEditing &&
+					<Fab color='secondary' aria-label='edit' onClick={() => setIsEditing(true)}>
+						<EditIcon />
+					</Fab>
+				}
+				{isEditing && <FilmPosterForm filmEPK={filmEPK} addFilmInfo={addFilmInfo} setPoster={setPoster} setIsEditing={setIsEditing} />}
+				{!isEditing && <FilmPosterDisplay filmEPK={filmEPK} poster={poster}/> }
+			</div> }
+		</div>
   )
 
 
--- src/components/FilmPoster/FilmPosterDisplay.tsx
+++ src/components/FilmPoster/FilmPosterDisplay.tsx
@@ -2,18 +2,20 @@ import { FilmEPK } from '../../types';
 
 interface IFilmPoster {
   filmEPK: FilmEPK;
+  poster: string
 }
 
-const FilmPosterDisplay = ({ filmEPK } : IFilmPoster) => {
+const FilmPosterDisplay = ({ filmEPK , poster} : IFilmPoster) => {
+
 
   return (
     <section>
       <p>I am the film poster display</p>
-      {filmEPK.attributes !== undefined && <img 
+       <img 
         className='poster'
-        src={filmEPK.attributes.movie_poster_url}
+        src={poster}
         alt=''
-      />}
+      />
     </section>  
   )
 }
