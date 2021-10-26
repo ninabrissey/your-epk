8a3f3e0118c4ceb330808a8c4653250f9f4ec7cf
Rachel Jensen <rachelangela@gmail.com>
2021-10-26 17:46:16 -0600

    Add get request to fix props bug on display

--- src/components/FilmPoster/FilmPosterDisplay.tsx
+++ src/components/FilmPoster/FilmPosterDisplay.tsx
@@ -1,19 +1,27 @@
 import { FilmEPK } from '../../types';
+import { useState, useEffect } from 'react';
+import { getEPK } from '../../utils/apiCalls';
 
 interface IFilmPoster {
-  filmEPK: FilmEPK;
-  poster: string
+  // filmEPK: FilmEPK;
+  // poster: string,
+  epk_id: string
 }
 
-const FilmPosterDisplay = ({ filmEPK , poster} : IFilmPoster) => {
+const FilmPosterDisplay = ({ epk_id} : IFilmPoster) => {
+  const [image, setImage] = useState<string>('')
 
+  useEffect(() => {
+    getEPK(epk_id)
+      .then(data => setImage(data.data.attributes.movie_poster_url))
+  })
 
   return (
     <section>
       <p>I am the film poster display</p>
        <img 
         className='poster'
-        src={poster}
+        src={image}
         alt=''
       />
     </section>  
