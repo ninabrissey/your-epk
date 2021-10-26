2ee599ed8aac5842044c723baca32a6b42573b8a
Maria DelSignore <maria.ad27@gmail.com>
2021-10-26 00:31:00 -0600

    Comment out console log

--- src/utils/cleanData.ts
+++ src/utils/cleanData.ts
@@ -2,5 +2,5 @@ import { Included } from "../types"
 
 export const filterIncluded = (included: Included[], type: string) => {
   return included.filter((item: Included) => {
-    console.log(item.id)
+    // console.log(item.id)
     return item.type === type})}
