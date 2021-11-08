729d07ee6fa11050c72504634b14134e66eddce5
Maria DelSignore <maria.ad27@gmail.com>
2021-11-07 19:41:41 -0700

    Link login and register components

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import Button from '@mui/material/Button';
-
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import { UserData } from '../../../types';
 
 const LoginPage = () => {
   const [isLoggingIn, setIsLoggingIn] = useState<boolean>(true)
-  const [isRegistering, setIsRegistering] = useState<boolean>(false)
+  const [isRegistering, setIsRegistering] = useState<boolean>(true)
 
   return (
     <section>
-      {!isRegistering && <LoginForm setIsRegistering={setIsRegistering} />}
-      {isRegistering && <RegisterForm />}
+      {!isRegistering && isLoggingIn && <div>
+        <p>Thank you for registering with Your EPK</p>
+        <p>Please login to go to your new account</p>
+      </div>}
+      {!isRegistering && !isLoggingIn && <RegisterForm setIsRegistering={setIsRegistering} setIsLoggingIn={setIsLoggingIn} />}
+      {isRegistering && isLoggingIn && <LoginForm setIsRegistering={setIsRegistering} setIsLoggingIn={setIsLoggingIn} />}
+      {!isRegistering && isLoggingIn && <LoginForm setIsRegistering={setIsRegistering} setIsLoggingIn={setIsLoggingIn} />}
     </section>
   )
 }
