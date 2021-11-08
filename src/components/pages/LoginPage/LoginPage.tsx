e3c550caafbdff53b186dd2c781d3b4bdf7d896d
Maria DelSignore <maria.ad27@gmail.com>
2021-11-07 17:28:34 -0700

    Create file for login form

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -3,42 +3,12 @@ import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 
+import LoginForm from '../LoginPage/LoginForm';
+
 const LoginPage = () => {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
 
   return (
-    <form className='login-container'>
-			<FormControl sx={{ m: 1, minWidth: 120 }}>
-				<TextField
-					id="outlined-basic"
-					label="Email"
-					variant="outlined"
-					size="small"
-					margin="dense"
-					type="text"
-					name="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					required
-				/>
-      	<TextField
-					id="outlined-basic"
-					label="Password"
-					variant="outlined"
-					size="small"
-					margin="dense"
-					type="password"
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					required
-				/>
-        <Button variant="text">
-					Login
-				</Button>
-      </FormControl>  
-    </form>
+   <LoginForm /> 
   )
 }
 
