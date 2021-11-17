19a36dc9bd873b5ce76cb5f848a7a0e6b09b1bc7
Maria DelSignore <maria.ad27@gmail.com>
2021-11-16 20:52:06 -0700

    Import film form and pass props

--- src/components/Filmteam/FilmTeamContainer.tsx
+++ src/components/Filmteam/FilmTeamContainer.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useState } from 'react';
+import { FilmEPK } from '../../types';
+import FilmTeamForm from './FilmTeamForm';
+import FilmTeamDisplay from './FilmTeamDisplay';
 
-const FilmTeamContainer = () => {
+interface IFilmTeam {
+	filmEPK: FilmEPK;
+  epk_id: string;
+	addFilmInfo: any;
+}
+
+const FilmTeamContainer = ({ filmEPK, epk_id, addFilmInfo } : IFilmTeam) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true);
+
   return (
-    <div>
-      
+    <div className='film-team-container'>
+      <h2>Film Crew</h2>
+      <FilmTeamForm 
+        filmEPK={filmEPK}
+        epk_id={epk_id}
+        addFilmInfo={addFilmInfo}
+        setIsEditing={setIsEditing} 
+      />
+      {/* <FilmTeamDisplay /> */}
     </div>
   )
 }
 
-export default FilmTeamContainer
+export default FilmTeamContainer;
