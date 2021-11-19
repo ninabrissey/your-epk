5857b97fd931cec166fd86b613ff71ee06c525e6
Rachel Jensen <rachelangela@gmail.com>
2021-11-19 11:48:47 -0700

    Update dash to get EPKs via session cookie

--- src/components/pages/DashboardPage/Dashboard/Dashboard.tsx
+++ src/components/pages/DashboardPage/Dashboard/Dashboard.tsx
@@ -4,22 +4,35 @@ import './Dashboard.scss';
 import TitleForm from '../TitleForm/TitleForm';
 import EPKContainer from '../EPKContainer/EPKContainer';
 import Navigation from '../../../Navigation/Navigation';
-import { UserData, FilmEPK } from '../../../../types';
+import { ThisIsUser, FilmEPK } from '../../../../types';
 import { setupMaster } from 'cluster';
 import { userInfo } from 'os';
+import Cookies from 'js-cookie';
 
-interface IUser {
-  id: number;
-  currUser: UserData;
-}
+// interface IUser {
+//   // id: number;
+//   currUser: ThisIsUser;
+// }
 
-const Dashboard = ({ id, currUser }: IUser) => {
+const Dashboard = ({ currUser }: any) => {
   const [allFilms, setAllFilms] = useState<FilmEPK[]>([]);
 
   useEffect(() => {
-    fetch(`https://epk-be.herokuapp.com/api/v1/users/${id}`)
+    const cookie: any = Cookies.get('csrf-token')
+
+    fetch(`https://epk-be.herokuapp.com/api/v2/user`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        "X-CSRF-Token": cookie
+      },  
+      credentials: "include"
+    })
       .then((res) => res.json())
       .then((data: any) => {
+        console.log(currUser)
         setAllFilms(data.included);
       })
       .catch((err) => console.log(err));
@@ -31,9 +44,9 @@ const Dashboard = ({ id, currUser }: IUser) => {
       <main className="edit-main">
         <div className="party-title">
           <h1>To build your <span>electronic press kit</span>, enter your film title below</h1>
-          <TitleForm allFilms={allFilms} id={id} setAllFilms={setAllFilms} />
+          <TitleForm allFilms={allFilms} setAllFilms={setAllFilms} />
         </div>
-        <EPKContainer allFilms={allFilms} setAllFilms={setAllFilms} />
+       <EPKContainer allFilms={allFilms} setAllFilms={setAllFilms} />
       </main>
     </div>
   );
