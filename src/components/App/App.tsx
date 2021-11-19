25f580ee8fe8dfaaeea26866a829b2a99ff41b65
Rachel Jensen <rachelangela@gmail.com>
2021-11-19 16:17:52 -0700

    Remove unused code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,15 +5,14 @@ import EditPage from '../pages/EditPage/EditPage';
 import PressPage from '../pages/PressPage/PressPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import LoginForm from '../pages/LoginPage/LoginForm';
+import Navigation from '../Navigation/Navigation';
 import RegisterForm from '../pages/LoginPage/RegisterForm';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
 import { ThisIsUser } from '../../types';
 
-// const userId: number = 1;
 
 function App() {
   const [currUser, setCurrUser] = useState<ThisIsUser>({} as ThisIsUser);
-
 
   return (
     <div className="App">
@@ -23,16 +22,16 @@ function App() {
           <Redirect to={`/login`} />
         </Route>
         <Route exact path={`/login`}>
+          <Navigation onLogin={true}/>
           <LoginForm setCurrUser={setCurrUser}/>
         </Route>
         <Route exact path={`/register`}>
           <RegisterForm setIsRegistering={true} setIsLoggingIn={true} />  
         </Route>
         <Route exact path={`/dashboard`}>
-    
-          <Dashboard currUser={currUser} setCurrUser={setCurrUser}/>
-    
-           {/* <Dashboard currUser={currUser}  />   */}
+
+          <Dashboard />
+
         </Route>
         <Route exact path='/edit/:epk_id' render={({ match }) =>
           <EditPage epk_id={match.params.epk_id} />
