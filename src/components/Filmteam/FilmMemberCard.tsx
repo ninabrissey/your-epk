d04fdf7ae1cbcba604642deb8e7ca2d8e61ed04f
Maria DelSignore <maria.ad27@gmail.com>
2021-11-19 17:19:39 -0700

    Remove unused code

--- src/components/Filmteam/FilmMemberCard.tsx
+++ src/components/Filmteam/FilmMemberCard.tsx
@@ -4,7 +4,6 @@ const FilmMemberCard = ({ name, role, description } : any) => {
 
   return (
     <article>
-      {/* <h4>I am the FILM MEMBER CARD</h4> */}
       <p>name: {name}</p>
       <p>role: {role}</p>
       <p>description: {description}</p>
