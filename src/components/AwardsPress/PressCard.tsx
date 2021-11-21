632577aff6277ddb0ed724b4d02db4230f036be7
Nina Brissey <nbrissey@gmail.com>
2021-11-21 10:47:36 -0700

    Remove unused className

--- src/components/AwardsPress/PressCard.tsx
+++ src/components/AwardsPress/PressCard.tsx
@@ -9,12 +9,9 @@ interface IPressCard {
   style: any;
 }
 
-// Need to adjust this so the text is showing up. Not sure what the
-// issue is so I copied over the exactly styling from the award card
-
 const PressCard = ({ press, style }: IPressCard) => {
   return (
-    <article style={style} className="award-press-card-background press-card">
+    <article style={style} className="press-card">
       {/* <Fab
         style={{ marginLeft: '235px', marginBottom: '235px' }}
         size="small"
