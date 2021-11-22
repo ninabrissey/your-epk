60579cfde5bf1f4fc8ad9a7a927139b7b75b5064
Maria DelSignore <maria.ad27@gmail.com>
2021-11-21 17:37:21 -0700

    Update film poster img to render on page load

--- src/components/FilmPoster/FilmPosterForm.tsx
+++ src/components/FilmPoster/FilmPosterForm.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { getPresignedUrl, putToAWS, postToDatabase } from '../../awsS3/helperFunctions';
 
 
-const FilmPosterDisplay = ({ filmEPK, setIsEditing }: any) => {
+const FilmPosterDisplay = ({ filmEPK, setIsEditing, isEditing }: any) => {
   const [filmPoster, setFilmPoster] = useState<any>({});
   const [reminder, setReminder] = useState<boolean>(false)
 
@@ -36,7 +36,7 @@ const FilmPosterDisplay = ({ filmEPK, setIsEditing }: any) => {
       <input id='test-input' type="file" accept="image/*" />
       {reminder && <p>Must choose a file to save</p>}
       <button onClick={(event) => { handleSubmit(event) }}>Save</button>
-      <button onClick={() => setIsEditing(false)} >Done editing</button>
+      <button onClick={() => setIsEditing(!isEditing)} >Done editing</button>
     </form>
   )
 }
