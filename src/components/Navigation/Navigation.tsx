da84e925c3dcd78965d48ebff229732cd5d42c6e
Nina Brissey <nbrissey@gmail.com>
2021-11-23 13:28:18 -0700

    Add ThemeSelector component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,22 +1,28 @@
+import ThemeSelector from '../../ThemeSelector/ThemeSelector';
 import './Navigation.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../images/EPK_LOGO.png';
 
 const Navigation = ({ onEdit, onLogin, epk_id, title }: any) => {
   return (
-    <nav className='navigation'>
+    <nav className="navigation">
       <Link to={`/dashboard`}>
-        <img className='logo' src={logo} alt='Your EPK logo' />
+        <img className="logo" src={logo} alt="Your EPK logo" />
       </Link>
-      {onEdit && <div className='menu'>
-        <Link to={`/dashboard`}>Home</Link>
-        <Link to={`/${epk_id}/${title}`}>Press Kit</Link>
-      </div>}
-      {onLogin && <div className='menu'>
-        <Link to={`/dashboard`}>Home</Link>
-      </div>}
+      {onEdit && (
+        <div className="menu">
+          <Link to={`/dashboard`}>Home</Link>
+          <Link to={`/${epk_id}/${title}`}>Press Kit</Link>
+          <ThemeSelector />
+        </div>
+      )}
+      {onLogin && (
+        <div className="menu">
+          <Link to={`/dashboard`}>Home</Link>
+        </div>
+      )}
     </nav>
   );
-}
+};
 
 export default Navigation;
