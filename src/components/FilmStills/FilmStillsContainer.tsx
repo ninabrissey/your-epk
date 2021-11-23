b81c7967ac598ed6b1170cc0a0ba1731f72bc39f
Nina Brissey <nbrissey@gmail.com>
2021-11-22 19:08:16 -0700

    Build out basic FilmStillsContainer

--- src/components/FilmStills/FilmStillsContainer.tsx
+++ src/components/FilmStills/FilmStillsContainer.tsx
@@ -1,25 +1,46 @@
-import { useState } from 'react';
-import { postData } from '../../utils/apiCalls';
-import { Included, Image } from '../../types';
+import { useState, useEffect } from 'react';
+import { postData, getArrayData } from '../../utils/apiCalls';
+// import { Included, Image } from '../../types';
+import { Image } from '../../types';
 import FilmStillsDisplay from './FilmStillsDisplay';
 import FilmStillsForm from './FilmStillsForm';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
-interface IFilmStillsContainer {
-  included: Included[] | [];
-  epk_id: number;
-}
-
-// const ImagesContainer = ({ currentImages, epk }: IImageConainer) => {
-const FilmStillsContainer = ({ included, epk_id }: IFilmStillsContainer) => {
-  const [isEditting, setIsEditting] = useState(false);
-  const [images, setImages] = useState<Image[] | []>([]);
+const FilmStillsContainer = (epk_id: any) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [filmStills, setFilmStills] = useState<Image[] | []>([]);
   const [error, setError] = useState<any>('');
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    getArrayData('film_stills', epk_id);
+  }, []);
+
   return (
     <section>
-      {/* <FilmStillsDisplay currentImages={images} epk_id={epk} /> */}
-      <FilmStillsForm />
+      <h2>Film Stills</h2>
+
+      {!isEditing && (
+        <Fab
+          size="small"
+          aria-label="edit"
+          onClick={() => setIsEditing(true)}
+          className="awards-press-edit-btn"
+        >
+          <EditIcon />
+        </Fab>
+      )}
+      <FilmStillsDisplay filmStills={filmStills} epk_id={epk_id} />
+      {isEditing && (
+        <FilmStillsForm
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          filmStills={filmStills}
+          setFilmStills={setFilmStills}
+          epk_id={epk_id}
+        />
+      )}
     </section>
   );
 };
