43f42eafe379ae3e96b43faadb95a4a80169fb6b
Rachel Jensen <rachelangela@gmail.com>
2021-11-24 17:53:16 -0700

    Add redirect for access denial based on cookie

--- src/components/pages/EditPage/EditPage.tsx
+++ src/components/pages/EditPage/EditPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { patchData, getEPK } from '../../../utils/apiCalls';
 import { FilmEPK, Included } from '../../../types';
 import AwardsPressContainer from '../../AwardsPress/AwardsPressContainer';
@@ -13,6 +14,7 @@ import TaglinesContainer from '../../Taglines/TaglinesContainer';
 // import ImagesContainer from '../../Images/ImagesContainer';
 // import ImagesForm from '../../Images/ImagesForm';
 import FilmTeamContainer from '../../Filmteam/FilmTeamContainer';
+import Cookies from 'js-cookie';
 
 const EditPage = ({ epk_id }: any) => {
   const [error, setError] = useState<any>('');
@@ -20,12 +22,22 @@ const EditPage = ({ epk_id }: any) => {
   const [film, setFilm] = useState<FilmEPK>({} as FilmEPK);
   const [title, setTitle] = useState('');
   const [included, setIncluded] = useState<Array<Included>>([]);
+  const [hasAccess, setHasAccess] = useState<boolean>(true)
 
   useEffect(() => {
     setLoading(true);
     getFilmData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [epk_id]);
+
+  useEffect(() => {
+    const cookie = Cookies.get('csrf-token');
+
+    if (!cookie) {
+      setHasAccess(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const getFilmData = () => {
     getEPK(epk_id)
@@ -50,7 +62,10 @@ const EditPage = ({ epk_id }: any) => {
 
   return (
     <div>
-      {error && <Error />}
+      {!hasAccess && (
+        <Redirect to={`/no-edit-access/${epk_id}`} />
+      )}
+      {error && <Error accessDenied={false} notFound={true} epk_id={0}/>}
       {loading ? (
         <p>Loading</p>
       ) : (
