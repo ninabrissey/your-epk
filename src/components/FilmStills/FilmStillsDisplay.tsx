1360dc44c88e4e6eeaeefab4d7f46ebca63b0f22
Nina Brissey <nbrissey@gmail.com>
2021-11-26 15:14:08 -0700

    Change classname, add props, remove comments

--- src/components/FilmStills/FilmStillsDisplay.tsx
+++ src/components/FilmStills/FilmStillsDisplay.tsx
@@ -1,34 +1,25 @@
+import { useEffect } from 'react';
 import FilmStillsCard from './FilmStillsCard';
-import { Image } from '../../types';
 
-const FilmStillsDisplay = ({ filmStills, epk_id }: any) => {
+const FilmStillsDisplay = ({
+  filmStills,
+  isEditing,
+  epk_id,
+  removeFilmMember,
+}: any) => {
   const filmsStillCards = filmStills.map((filmStill: any) => {
-    return <FilmStillsCard key={filmStill.id} filmStill={filmStill} />;
+    return (
+      <FilmStillsCard
+        key={filmStill.id}
+        id={filmStill.id}
+        filmStill={filmStill}
+        removeFilmMember={removeFilmMember}
+        isEditing={isEditing}
+      />
+    );
   });
 
-  return <div className="image-display-container">{filmsStillCards}</div>;
+  return <div className="all-crew-wrapper">{filmsStillCards}</div>;
 };
 
 export default FilmStillsDisplay;
-
-// const filmStills: Image[] = [
-//   {
-//     id: 1,
-//     film_epk_id: 133,
-//     link: 'https://images.squarespace-cdn.com/content/v1/594ea6feccf210e3df36ce8d/1530550829458-1E1T99Y53C81CU09S9P3/Nina+Brissey+and+Robert+Craighead+Amazing+Grace+Film',
-//     description: 'Behind the scenes - Robert Craighead and Nina Brissey',
-//   },
-//   {
-//     id: 11,
-//     film_epk_id: 133,
-//     link: 'https://m.media-amazon.com/images/M/MV5BZmRlNzQwOWUtODk1NC00YjA5LTk2YTQtODFlOGVkNGM2NzFlXkEyXkFqcGdeQXVyMjcwOTg2MzY@._V1_.jpg',
-//     description:
-//       'Behind the scenes - Katherine Kampko, Nina Brissey, and Robert Craighead',
-//   },
-//   {
-//     id: 14,
-//     film_epk_id: 133,
-//     link: 'http://static1.squarespace.com/static/594ea6feccf210e3df36ce8d/t/5b484d9488251b39eb13687b/1531465151647/Nina+Brissey+and+Robert+Craighead+Amazing+Grace+.jpg?format=1500w',
-//     description: 'Robert Craighead',
-//   },
-// ];
